b88c67834a0dfe7b4eb8b6412c3d2196033018f0
Kaplaugher <kaplaugher94@gmail.com>
2017-06-13 16:01:10 -0400

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    <h1>The Home Component</h1>
+</div>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>The Stock Component</h1>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
